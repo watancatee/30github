c0bf19cf2fd2a60dff62b401c4c171f9b9235831
watancatee <daryn.daniyaruly11@gmail.com>
2025-02-25 23:08:50 +0500

    Tried coding something from a while

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+print("Write a number")
+a = int(input())
+print("Is " + str(a) + " your number")
+answer = input().lower()
+if answer == "yes":
+    print("Great!")
+else:
+    exit()
+if a > 0:
+    print("Your number is greater than 0! Congrats")
